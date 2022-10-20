d750c363f4a0b69f3eab0dc5e6d56d2bbc4cb3e1
eunhyung <ooooo5h91@gmail.com>
2022-10-20 14:45:53 +0900

    [ADD] 로그인

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from users.views import KakaoOauthView, SignUpView, UserAdditionalInfoView
+from users.views import KakaoOauthView, SignUpView, SignInView, UserAdditionalInfoView
 
 urlpatterns = [
     path('/kakao/oauth', KakaoOauthView.as_view()),
     path('/signup', SignUpView.as_view()),
+    path('/signin', SignInView.as_view()),
     path('/additional-info', UserAdditionalInfoView.as_view())
 ]
