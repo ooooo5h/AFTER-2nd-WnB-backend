606569ca495923e6d13a390d967e8047557eb2fa
eunhyung <ooooo5h91@gmail.com>
2022-10-20 22:58:02 +0900

    [ADD] 호스트 집 등록하기

--- hosts/urls.py
+++ hosts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from hosts.views import RegisterHostView
+from hosts.views import RegisterHostView, RegisterRoomView
 
 urlpatterns = [
-    path('', RegisterHostView.as_view())
+    path('', RegisterHostView.as_view()),
+    path('/rooms', RegisterRoomView.as_view())
 ]
