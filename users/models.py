768bc30d257ded8c44b1cc8230050e112955d9f5
eunhyung <ooooo5h91@gmail.com>
2022-10-20 11:31:56 +0900

    [ADD] 회원가입

--- users/models.py
+++ users/models.py
@@ -4,14 +4,15 @@ from django.db      import models
 from core.models    import TimeStampModel
 
 class User(TimeStampModel):
-    first_name          = models.CharField(max_length = 50, null=True)
-    last_name           = models.CharField(max_length = 50, null=True)
-    email               = models.CharField(max_length = 50)
-    kakao_id            = models.BigIntegerField(unique=True)
-    kakao_profile_img   = models.CharField(max_length = 500)
-    phone_number        = models.CharField(max_length = 50, null=True)
-    birth_day           = models.CharField(max_length = 50, null=True)
-    point               = models.DecimalField(max_digits = 10, decimal_places = 2, default=Decimal('100000.00'))
+    first_name        = models.CharField(max_length = 50, null=True)
+    last_name         = models.CharField(max_length = 50, null=True)
+    email             = models.CharField(max_length = 50, unique=True)
+    password          = models.CharField(max_length = 500, null=True)
+    kakao_id          = models.BigIntegerField(unique=True)
+    kakao_profile_img = models.CharField(max_length = 500, null=True)
+    phone_number      = models.CharField(max_length = 50, null=True)
+    birth_day         = models.CharField(max_length = 50, null=True)
+    point             = models.DecimalField(max_digits = 10, decimal_places = 2, default=Decimal('100000.00'))
         
     class Meta:
         db_table = 'users'
--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from users.views import KakaoOauthView, UserAdditionalInfoView
+from users.views import KakaoOauthView, SignUpView, UserAdditionalInfoView
 
 urlpatterns = [
     path('/kakao/oauth', KakaoOauthView.as_view()),
+    path('/signup', SignUpView.as_view()),
     path('/additional-info', UserAdditionalInfoView.as_view())
 ]
